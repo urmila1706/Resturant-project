beda3c1d396787aedbe88b312a14825d07644a1b
urmila1706 <raut.urmila88@gmail.com>
2024-04-17 23:14:02 +0530

    Cart open and close

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,14 +4,14 @@ const Cart=(props)=>{
     const cartItems= <ul className={classes['cart-items']}>{[
         {id :'c1', name : 'Alaka',amount : '2' ,price:'12.99'}]
     .map(item=><li>{item.name}</li>)}</ul>
-return <Modal>
+return <Modal onClose={props.onClose}>
     {cartItems}
     <div className={classes.total}>
         <span>Total Amount</span>
         <span>34.89</span>
     </div>
     <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
     </div>
     </Modal>
--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,12 +1,12 @@
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 const HeaderCartButton=props=>{
-    return <button className={classes.button}>
+    return <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon/>
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>0</span>
+        <span className={classes.badge}>4</span>
     </button>
 };
 export default HeaderCartButton;
